42e31d3a7eed9adc4126dc77c97057f98e9afa91
Janindu Tharinda <janindu.the.dev@gmail.com>
2026-05-27 19:34:00 +0530

    add Car Rental System github link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ export default function Projects() {
       gallery: ["/car.png", "/car-internal.png", "/car-dashboard.png"],
       desc: "A complete car rental solution with real-time booking and admin controls. Built with MERN stack for high performance and scalability.",
       link: "https://mern-car-rental-system-1.onrender.com",
-      github: "#",
+      github: "https://github.com/janinduthedev/mern-car-rental-system",
     },
     {
       id: 2,
